f1001ca5e32e760f704aaa7b98023de619f95f4c
yuyunliao0821 <53509008+yuyunliao0821@users.noreply.github.com>
2022-03-15 18:45:49 +0800

    update files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from service_tools.service import Predictor, Preprocessor
 
 app = Flask(__name__)
@@ -12,17 +12,23 @@ predictor = Predictor()
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     print(request.args)
+#     if 'text' in request.args:
+#         text = request.args['text']
+#     if 'mlmodel' in request.args:
+#         mlmodel = request.args['mlmodel']
+#         print(mlmodel)
+    
+#     return text+mlmodel
+@app.route('/predict', methods=['POST'])
 def predict():
+    result = request.get_json()[0]
+    result = result['text']+result['mlmodel']
+    print(result)
 
-    mlmodel = request.form['mlmodel']
-    text = request.form['text']
-    doc_vec = preprocessor.get_doc_vec(text)
-    sentiment = predictor.predict_sentiment(mlmodel, doc_vec)
-
-    return render_template('index.html',entered_text = "Entered Review:",
-    original_text = text, prediction_text=sentiment)
-
+    return jsonify(result)
 
 if __name__ == '__main__':
     # run server
--- static/js/predict_sentiment.js
+++ static/js/predict_sentiment.js
@@ -1,3 +1,26 @@
 function predict_sentiment() {
-    text = "test"
-  }
+  
+  var inputText = document.getElementById("textbox").value;
+  var selectedModel = document.getElementById('ml-model').value;
+  var predictionContainer = document.getElementById("prediction");
+  var predictionText = document.getElementById("prediction-text")
+
+  predictionContainer.style.display = "flex";
+
+  var server_data=[{
+    'text':inputText,
+    'mlmodel':selectedModel
+  }]
+
+  $.post({
+    url:"/predict", 
+    data:JSON.stringify(server_data), 
+    contentType:'application/json'}).done(function (data){
+  predictionText.innerHTML = data;
+  });
+
+  //  $.get("/predict?"+"text="+inputText+"&"+"mlmodel="+selectedModel).done(function (data) {
+  //    predictionText.innerHTML = data;
+  //  })
+
+}
