e6d3936fa636e1dcb2e8e6c65521fbb8bad22624
yuyunliao0821 <53509008+yuyunliao0821@users.noreply.github.com>
2022-03-18 16:31:00 +0800

    update html,js

--- static/js/predict_sentiment.js
+++ static/js/predict_sentiment.js
@@ -1,31 +1,35 @@
-function predictSentiment() {
+window.onload = function(){
+  function predictSentiment() {
   
-  var inputText = document.getElementById("textbox").value;
-  var selectedModel = document.getElementById('ml-model').value;
-  var predictionContainer = document.getElementById("prediction");
-  var predictionText = document.getElementById("prediction-text");
-  var originalText = document.getElementById("original-text")
-
-  predictionContainer.style.display = 'flex';
-  predictionText.style.display= 'None'
-
-  var serverData=[{
-    'text':inputText,
-    'mlmodel':selectedModel
-  }]
-
-  $.post({
-    url:"/predict", 
-    data:JSON.stringify(serverData), 
-    contentType:'application/json'}).done(function (data){
-      predictionText.style.display = 'flex';
-      predictionText.innerHTML = data;
-      originalText.innerHTML = inputText;
-    }
-  );
-
-  //  $.get("/predict?"+"text="+inputText+"&"+"mlmodel="+selectedModel).done(function (data) {
-  //    predictionText.innerHTML = data;
-  //  })
-
+    var inputText = document.getElementById("textbox").value;
+    var selectedModel = document.getElementById('ml-model').value;
+    var predictionContainer = document.getElementById("prediction");
+    var predictionText = document.getElementById("prediction-text");
+    var originalText = document.getElementById("original-text")
+  
+    predictionContainer.style.display = 'flex';
+    predictionText.style.display= 'None'
+  
+    var serverData=[{
+      'text':inputText,
+      'mlmodel':selectedModel
+    }]
+  
+    $.post({
+      url:"/predict", 
+      data:JSON.stringify(serverData), 
+      contentType:'application/json'}).done(function (data){
+        predictionText.style.display = 'flex';
+        predictionText.innerHTML = data;
+        originalText.innerHTML = inputText;
+      }
+    );
+  
+    //  $.get("/predict?"+"text="+inputText+"&"+"mlmodel="+selectedModel).done(function (data) {
+    //    predictionText.innerHTML = data;
+    //  })
+  }
+  
+  var button = document.getElementById('button')
+  button.addEventListener('click', predictSentiment)
 }
